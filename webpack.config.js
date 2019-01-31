411776d1f96816183789f8c7c27ddeb6cab188fe
Lucas Silva <lucas.silva@avecbrasil.com.br>
2019-01-31 17:06:06 -0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+    // Change to your "entry-point".
+    entry: path.resolve(__dirname, 'src'),
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    'externals': {
+        // don't bundle the 'react' npm package with our bundle.js
+        // but get it from a global 'React' variable
+        'react': 'React',
+        "react-dom": "ReactDOM"
+    },
+    module: {
+        rules: [{
+            // Include ts, tsx, js, and jsx files.
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            loader: ['babel-loader','ts-loader'],
+        }],
+    }
+};
